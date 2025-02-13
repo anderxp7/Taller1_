9cb9914f901a50fb02bec503cb5107c5f001c741
anderxp7 <andresfelipeps483@gmail.com>
2025-02-13 08:15:54 -0500

    Add files via upload

---
+++ exe1.py
@@ -0,0 +1,32 @@
+
+print ("Dime un numero")
+primerNumero = int(input())
+print ("Dime otro numero")
+segundoNumero = int(input())
+print ("dime de cuanto es el Descuento")
+Descuento = int(input())
+
+print ("Su suma es")
+print (primerNumero + segundoNumero)
+
+print ("Su resta es")
+print (primerNumero - segundoNumero)
+
+print ("Su multiplicacion es")
+print (primerNumero * segundoNumero)
+
+print ("Su division es")
+
+print (primerNumero + segundoNumero)
+
+print ("Su porcentaje es")
+print (primerNumero % segundoNumero)
+
+print ("Su valor con descuento es")
+Descuento1 = ((Descuento /100) * (primerNumero + segundoNumero) )
+
+print ("Su valor total con descuento es")
+print ((primerNumero + segundoNumero) - Descuento1 )
+
+print ( (((((primerNumero + segundoNumero) + 5) - 3) * 2) /4) )
+
